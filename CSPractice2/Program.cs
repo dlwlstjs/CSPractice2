49b2c3b03cf09a6051fa387fc6a2d5938ecfd9d0
dlwlstjs <dlwlstjs0413@gmail.com>
2024-07-03 09:19:24 +0900

    #6 23-8. 오버라이딩

--- CSPractice2/Program.cs
+++ CSPractice2/Program.cs
@@ -10,18 +10,25 @@ namespace CSPractice312
         class Parent
         {
             public int variable = 273;
-            public void Method()
+            public virtual void Method()
             {
                 Console.WriteLine("부모 메서드");
             }
         }
-
         class Child : Parent
         {
             public new string variable = "하이딩";
             public new void Method()
             {
                 Console.WriteLine("자식 메서드");
+            }
+        }
+
+        class Child2 : Parent
+        {
+            public override void Method()
+            {
+                Console.WriteLine("오브라이드된 자식 메서드");
             }
         }
 
@@ -32,7 +39,6 @@ namespace CSPractice312
             // #6 23-7. 새도잉
             int number = 20;
             Console.WriteLine(number); // 20
-
             // #6 23-7. 하이딩
             Child child = new Child();
             Console.WriteLine(child.variable); // 출력: 하이딩
@@ -43,6 +49,14 @@ namespace CSPractice312
             Console.WriteLine(p.variable); // 출력: 273
             p.Method(); // 출력: 부모 메서드
             ((Child)p).Method(); // 출력: 자식 메서드
+
+            // #6 23-8. 오버라이딩
+            Child2 child2 = new Child2();
+            child2.Method();  // 출력: 오버라이드된 자식 메서드
+            ((Parent)child2).Method(); // 출력: 오버라이드된 자식 메서드
+            Parent p2 = child2;
+            p2.Method(); // 출력: 오버라이드된 자식 메서드
+            ((Child2)p2).Method(); // 출력: 오버라이드된 자식 메서드
         }
     }
 }
